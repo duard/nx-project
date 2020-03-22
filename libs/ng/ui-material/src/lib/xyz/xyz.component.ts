03661ed41a3a1348f888ced5a8c78e574af3b8c3
Carlos Sigpharma <duardbr@gmail.com>
2020-03-22 19:11:23 +0000

    Debug funcionou parcialmente

--- libs/ng/ui-material/src/lib/xyz/xyz.component.ts
+++ libs/ng/ui-material/src/lib/xyz/xyz.component.ts
@@ -7,10 +7,20 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class XyzComponent implements OnInit {
+  variavelXYZ = 'Goiaba';
 
   constructor() { }
 
   ngOnInit(): void {
+    if (this.variavelXYZ === 'Goiaba') {
+      console.log('3===', 'goiabou 3');
+    }
+
+    if (this.variavelXYZ == 'Goiaba') {
+      console.log('2==', 'goiabou 2');
+    }
+
+    console.log('xyz', 'Debugando', this.variavelXYZ);
   }
 
 }
