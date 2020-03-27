2cdd5b2c7d0a78ceed7829983887fec2ddc2e231
Carlos - Casa <duardbr@gmail.com>
2020-03-27 00:25:05 +0000

    Carlos - try database

--- libs/ng/ui-material/src/lib/xyz/xyz.component.ts
+++ libs/ng/ui-material/src/lib/xyz/xyz.component.ts
@@ -1,19 +1,26 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'nx-solsig-xyz',
   templateUrl: './xyz.component.html',
   styleUrls: ['./xyz.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class XyzComponent implements OnInit {
   variavelXYZ = 'Goiaba';
+
+  variavel : Observable<any>;
   // https://christiankohler.net/reactive-angular-with-ngrx-component
   count = 0;
 
-  constructor() {}
+  constructor(private http: HttpClient) {
+  }
 
   ngOnInit(): void {
+    this.fetch();
+
     if (this.variavelXYZ === 'Goiaba') {
       console.log('3===', 'goiabou 3');
     }
@@ -27,4 +34,36 @@ export class XyzComponent implements OnInit {
   }
   // also triggers change detection
   noEffect() {}
+
+  async fetch() {
+    this.variavel = await this.http.get<any>('/api');
+    const value = <any>await this.http.get<any>('/api');
+
+    value.subscribe(data => {
+      this.variavelXYZ = data;
+      console.log('=> ', this.variavelXYZ);
+    });
+
+    console.log('=> ', this.variavelXYZ, value);
+
+    this.variavel.subscribe(data => {
+      this.variavelXYZ = data;
+      console.log('=> ', this.variavelXYZ);
+    });
+
+    this.http.get<any>('/api').toPromise().then(data => {
+      this.variavelXYZ = data;
+      console.log('Promise resolved.')
+    });
+    console.log('I will not wait until promise is resolved..', this.variavelXYZ);
+
+     await this.http.get<any>('/api').toPromise().then(data => {
+      this.variavelXYZ = data;
+      console.log('acabou', data);
+    });
+
+    console.log('No issues, I will wait until promise is resolved..', this.variavelXYZ);
+    // this.variavelXYZ = 'Bosta';
+    console.log('=> ', this.variavelXYZ, value);
+  }
 }
